15eb9b5065d1e153bb79931a50f11142abbb0fc2
tiff nogueira <tiffnogueira@gmail.com>
2017-11-14 17:37:00 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const app = {};
+const colours = [];
+
+app.getColorIndex = (color) => {
+    return $.ajax({
+        url : `https://fun-fun-colors.herokuapp.com/colorcheck`,
+        method: 'GET',
+        dataType : 'json',
+        data: {
+            q: color
+        }
+    });
+};
+
+app.getColor = (id) => {
+    return $.ajax({
+        url: `https://fun-fun-colors.herokuapp.com/color/${id}`,
+        method: 'GET',
+        dataType: 'json',
+    });
+};
+
+app.setColor = async function(color) {
+    let colorId = await app.getColorIndex(color);
+    colorId = colorId.colorIndex;
+    const colorName = await app.getColor(colorId)
+    app.displayColor(colorName.color);
+}
+
+app.displayColor = function(color) {
+    $('.colors').empty();
+    $('body').css('background', color);
+    const colorName = $('<p>').addClass('colorName').text(color);
+    $('.colors').append(colorName);		
+};
+
+app.events = function() {
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const colorToSearch = $(this).find('input[name=find]').val().toLowerCase();
+        app.setColor(colorToSearch);
+    });
+}
+
+app.init = function() {
+    app.events();
+}
+
+$(app.init);
